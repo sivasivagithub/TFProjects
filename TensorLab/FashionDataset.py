e78151bf82c3c6f2d45474dc912df10447c3d03b
sivasiv@gmail.com <githubsiva123!>
2024-05-12 15:55:20 +0530

    Commiting TF Project

--- TensorLab/FashionDataset.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-from os import path, getcwd, chdir
-
-def train_mnist():
-
-    mnist = tf.keras.datasets.mnist
-
-    (x_train, y_train),(x_test, y_test) = mnist.load_data(path=path)
--- TensorLab/Horse2.py
+++
@@ -1,2 +0,0 @@
-
-#!wget --no-check-certificate \"https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \-O "/tmp/happy-or-sad.zip"
